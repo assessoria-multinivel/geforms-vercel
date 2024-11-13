23e8e4c2efa9fa6f7743d289addf67b79a67b6ff
Diego Patrick da Silva Santos <diegosantos.developer@gmail.com>
2024-11-13 18:31:44 -0300

    Commit de porta

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 3001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule 
@@ -22,6 +22,6 @@ async function bootstrap() {
   });
 
   console.log('NODE_ENV', process.env.NODE_ENV);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
